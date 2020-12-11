fc9535c4af92760d5d3db948cb652b1d9a131a56
ohyeaho <75355743+ohyeaho@users.noreply.github.com>
2020-12-11 07:51:46 +0000

    DRY/validation/add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_095853) do
+ActiveRecord::Schema.define(version: 2020_12_10_022922) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
