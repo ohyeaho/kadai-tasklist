1586624166e8e67ed7067ac42956a2d353761e2d
ohyeaho <75355743+ohyeaho@users.noreply.github.com>
2020-12-21 12:44:03 +0000

    task-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
@@ -16,7 +17,6 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-    
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
       redirect_to @task
@@ -55,5 +55,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status, :user)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:create]
   
-  resources :tasks
+  resources :tasks, only: [:show, :new, :create, :edit, :update, :destroy]
 end
