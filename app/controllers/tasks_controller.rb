2962823395458cb3ae42a5d14bb008c04b3569f4
ohyeaho <75355743+ohyeaho@users.noreply.github.com>
2020-12-21 13:12:37 +0000

    task-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
@@ -44,14 +42,10 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status, :user)
